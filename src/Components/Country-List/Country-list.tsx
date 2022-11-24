46b0d5e8e4040c8087c4f39cb2627582ef94466e
Arslan Okenov <gtg100900@gmail.com>
2022-11-25 03:40:10 +0600

    deleted unused files and created list and item components

---
+++ src/Components/Country-List/Country-list.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CountryItem from "../Country-Item/Country-item";
+
+const CountryList = () => {
+  return (
+    <div>
+      list
+      <CountryItem/>
+    </div>
+  );
+};
+
+export default CountryList;
