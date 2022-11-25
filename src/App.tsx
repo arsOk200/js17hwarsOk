81130bd0249cd9991d5f99b329b0ac66a9350c4f
Arslan Okenov <gtg100900@gmail.com>
2022-11-25 15:34:30 +0600

    deleted country list

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import CountryList from "./Components/Country-List/Country-list";
+import React, {useCallback, useEffect, useState} from 'react';
 import InfoBlock from "./Components/Info-Block/Info-Block";
+import CountryItem from "./Components/Country-Item/Country-item";
+import {Countries} from "./Components/type";
+import axios from "axios";
+
+const CountryNameURL= 'https://restcountries.com/v2/all?fields=name';
 
 function App() {
+  const [countries, setCountries] = useState<Countries[]>([]);
+  const [clickedCountry, setClickedCountry] = useState<string | null>(null);
+
+
+  const fetchData = useCallback(async () => {
+    const countriesResponse = await axios.get<Countries[]>(CountryNameURL);
+    const promises = countriesResponse.data.map(async country => {
+      return {
+        name:country.name,
+        id:Math.random().toString(),
+      }
+    });
+    const newCountries = await Promise.all(promises);
+    setCountries(newCountries);
+  }, []);
+
+  useEffect(()=> {
+    fetchData().catch(console.error);
+  }, [fetchData]);
+
+
   return (
     <div className="m-auto d-flex" style={{width:'890px'}}>
-      <CountryList/>
+      <div className="Country-List w-25">
+        <h1>Countries</h1>
+        {countries.map(country => (
+          <CountryItem name={country.name} key={country.id} onClick={() => setClickedCountry(country.name)}/>
+        ))}
+      </div>
       <div className="info-block w-75">
         <InfoBlock/>
       </div>
-
     </div>
   );
 }
--- src/Components/Info-Block/Info-Block.tsx
+++ src/Components/Info-Block/Info-Block.tsx
@@ -3,7 +3,17 @@ import React from 'react';
 const InfoBlock = () => {
   return (
     <>
-        info
+        <h1>Country name: </h1>
+        <p>Capital</p>
+      <img src="" alt="flag"/>
+      <p>Population:</p>
+      <div className="borders">
+        <h2>Borders</h2>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+        </ul>
+      </div>
     </>
   );
 };
